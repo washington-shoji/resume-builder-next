b3b6afd60404320f15d9b7ebe0f272fe747302c5
washington-shoji <shoji.washington@gmail.com>
2024-08-05 18:48:20 +1000

    - updated resume data type

--- src/app/types/resume-data.types.ts
+++ src/app/types/resume-data.types.ts
@@ -1,35 +1,40 @@
 export interface ResumeOneFormInput {
-	fullName: string;
-	roleTitle: string;
-	summary: string;
-	company: string;
-	timePeriod: string;
-	responsibilities: string;
-	icon: string;
-	label: string;
-	year: string;
-	educationTitle: string;
-	institution: string;
+	personalData?: {
+		heading?: Heading;
+		contact?: Contact;
+	};
+	professionalData?: Professional;
+	skillData?: Skill;
+	educationalData?: Education;
 }
 
 interface Heading {
-	fullName: string;
-	roleTitle: string;
+	fullName?: string;
+	roleTitle?: string;
+	summary?: string;
+}
+
+interface Contact {
+	email?: string;
+	phoneNumber?: string;
+	githubLink?: string;
+	linkedInLink?: string;
+	personalWebsiteLink?: string;
 }
 
 interface Professional {
-	company: string;
-	timePeriod: string;
-	responsibilities: string;
+	company?: string;
+	timePeriod?: string;
+	responsibilities?: string;
 }
 
 interface Skill {
-	icon: string;
-	label: string;
+	icon?: string;
+	label?: string;
 }
 
 interface Education {
-	year: string;
-	educationTitle: string;
-	institution: string;
+	year?: string;
+	educationTitle?: string;
+	institution?: string;
 }
