ffd7219e7333e9542e584960fccb59920acca0d7
washington-shoji <shoji.washington@gmail.com>
2024-08-08 11:55:02 +1000

    - updated resume preview page

--- src/app/(app)/preview/page.tsx
+++ src/app/(app)/preview/page.tsx
@@ -3,6 +3,8 @@
 import Resume from '@/app/components/Resume';
 import { ResumeOneFormInput } from '@/app/types/resume-data.types';
 import { useRef } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import { FaFilePdf } from 'react-icons/fa6';
 import { useReactToPrint } from 'react-to-print';
 
 interface Props {
@@ -14,27 +16,34 @@ export default function Preview(props: Props) {
 	const handlePrint = useReactToPrint({
 		content: () => componentRef.current,
 	});
+
+	function onDeleteData(): void {
+		localStorage.removeItem('form-data');
+		window.location.reload();
+	}
+
 	return (
-		<main className='flex justify-center bg-white dark:bg-gray-900'>
-			{/* <button
-						className='inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'
-						type='button'
-						onClick={handlePrint}
-					>
-						Print resume
-						<svg
-							className='w-5 h-5 ml-2 -mr-1'
-							fill='currentColor'
-							viewBox='0 0 20 20'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								fillRule='evenodd'
-								d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
-								clipRule='evenodd'
-							></path>
-						</svg>
-					</button> */}
+		<main className='flex flex-col gap-4 bg-white dark:bg-gray-900'>
+			<div className='flex flex-col gap-2 px-4 mt-4'>
+				<button
+					className='w-full inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'
+					type='button'
+					onClick={handlePrint}
+				>
+					Print resume
+					<FaFilePdf className=' text-xl ml-2' />
+				</button>
+
+				<button
+					className='w-full inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900'
+					type='button'
+					onClick={onDeleteData}
+				>
+					Delete resume data
+					<FaTrashAlt className=' text-xl ml-2' />
+				</button>
+			</div>
+
 			<div>
 				<Resume formData={props.formData} ref={componentRef}></Resume>
 			</div>
