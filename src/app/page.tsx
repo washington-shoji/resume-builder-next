0dc13c15cb409b3b1c341b7b75720f1b92ee79ce
washington-shoji <shoji.washington@gmail.com>
2024-09-08 09:45:53 +1000

    - updated index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
 			<div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
 				<div className='mr-auto place-self-center lg:col-span-7'>
 					<h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
-						Code.Sydney Resume Builder
+						Personal Resume Builder
 					</h1>
 					<p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
 						Build your own personal resume with beautiful templates.
 					</p>
 					<a
-						href='/resume-builder'
+						href='/resume-selection'
 						className='inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'
 					>
 						Start building
