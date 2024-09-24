b9b07daed2d930c4ca8cdd1acef28b15713b2e99
washington-shoji <shoji.washington@gmail.com>
2024-09-24 17:58:55 +1000

    - updated the resume selection page

--- src/app/(app)/resume-selection/page.tsx
+++ src/app/(app)/resume-selection/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 import { CiBookmarkCheck } from 'react-icons/ci';
 
@@ -10,33 +11,35 @@ export type Template = {
 	image: string;
 };
 
+const templates: Template[] = [
+	{
+		templateId: 'tmpl-1',
+		name: 'template-1',
+		image:
+			'https://cdn.dribbble.com/userupload/9266643/file/original-ed3b86b8c7318f5288fa72dd0fd72412.jpg?resize=1200x800&vertical=center',
+	},
+	{
+		templateId: 'tmpl-2',
+		name: 'template-2',
+		image:
+			'https://cdn.dribbble.com/userupload/9769018/file/original-073834e4a2c80fbbf1c3d48a6dc5089f.jpg?resize=1200x800&vertical=center',
+	},
+	{
+		templateId: 'tmpl-3',
+		name: 'template-3',
+		image:
+			'https://cdn.dribbble.com/userupload/5833158/file/original-5bb65ee037363a1cbe56500c0fb055c8.jpg?resize=1200x800',
+	},
+];
+
 export default function ResumeSelection() {
-	const [selected, setSelected] = useState<string | undefined>(undefined);
+	const [selected, setSelected] = useState<string | undefined>(
+		templates[0].templateId
+	);
 
 	function isSelected(id: string): boolean {
 		return selected === id;
 	}
-
-	const templates: Template[] = [
-		{
-			templateId: 'tmpl-1',
-			name: 'template-1',
-			image:
-				'https://cdn.dribbble.com/userupload/9266643/file/original-ed3b86b8c7318f5288fa72dd0fd72412.jpg?resize=1200x800&vertical=center',
-		},
-		{
-			templateId: 'tmpl-2',
-			name: 'template-2',
-			image:
-				'https://cdn.dribbble.com/userupload/9769018/file/original-073834e4a2c80fbbf1c3d48a6dc5089f.jpg?resize=1200x800&vertical=center',
-		},
-		{
-			templateId: 'tmpl-3',
-			name: 'template-3',
-			image:
-				'https://cdn.dribbble.com/userupload/5833158/file/original-5bb65ee037363a1cbe56500c0fb055c8.jpg?resize=1200x800',
-		},
-	];
 
 	return (
 		<main className='flex justify-center items-center min-h-screen bg-white dark:bg-gray-900 p-4 sm:p-0'>
@@ -58,17 +61,28 @@ export default function ResumeSelection() {
 						<div className=' flex flex-col sm:flex-row gap-4'>
 							{templates.map((t) => {
 								return (
-									<div key={t.templateId}>
+									<div
+										key={t.templateId}
+										className={
+											isSelected(t.templateId)
+												? ' border-[3px] border-blue-500 rounded-md'
+												: 'border-[3px] border-transparent rounded-md'
+										}
+									>
 										{isSelected(t.templateId) && (
-											<CiBookmarkCheck className=' text-blue-500 text-2xl absolute mt-1 ml-1 stroke-1' />
+											<Link
+												className='absolute mt-1 ml-1 inline-flex items-center justify-center gap-1 px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'
+												href={{
+													pathname: '/builder',
+													query: { id: t.templateId, name: t.name },
+												}}
+											>
+												<CiBookmarkCheck className=' text-lg' />
+												<span>Select</span>
+											</Link>
 										)}
 
 										<Image
-											className={
-												isSelected(t.templateId)
-													? ' border-[3px] border-blue-500 rounded-md'
-													: 'border-[3px] border-transparent rounded-md'
-											}
 											width={200}
 											height={200}
 											src={t.image}
