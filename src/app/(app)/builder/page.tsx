93769f4947ffda1a9b276a466d56169872a9dfdf
washington-shoji <shoji.washington@gmail.com>
2024-09-27 08:23:11 +1000

    - updated builder page

--- src/app/(app)/builder/page.tsx
+++ src/app/(app)/builder/page.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import BuilderStepper from '@/app/components/BuilderStepper';
 
 export default function Builder() {
-	return <div>Builder</div>;
+	const [currentStep, setCurrentStep] = useState<number>(0);
+	const numberOfSteps = 3;
+	const stepTitle = ['Personal', 'Professional', 'Educational'];
+	return (
+		<main className='flex justify-center min-h-screen bg-white dark:bg-gray-900'>
+			<div className='grid grid-cols-2 gap-6'>
+				<div className=' flex flex-col'>
+					<BuilderStepper
+						currentStep={currentStep}
+						numberOfSteps={numberOfSteps}
+						stepTitle={stepTitle}
+					></BuilderStepper>
+				</div>
+				<div className=' flex flex-col'>
+					<div>Resume builder</div>
+				</div>
+			</div>
+		</main>
+	);
 }
