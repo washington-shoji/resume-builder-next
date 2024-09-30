03cbacc9c87500cb390abc04225915388782d8bb
washington-shoji <shoji.washington@gmail.com>
2024-09-30 20:55:57 +1000

    - updated index title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'Code.Sydney Resume Builder',
+	title: 'Resume Builder',
 	description: 'Create beautiful resumes',
 };
 
