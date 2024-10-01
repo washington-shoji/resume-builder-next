85b69d39fa29354693283f6d667a18a285cce66e
washington-shoji <shoji.washington@gmail.com>
2024-10-01 11:36:20 +1000

    - updated resume professionaldetails component

--- src/app/components/ResumeProfessionalDetails.tsx
+++ src/app/components/ResumeProfessionalDetails.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import WorkExpFieldArray from './WorkExpFieldArray';
-import SkillFieldArray from './SkillFieldArray';
+import WorkExpFieldArray from "./WorkExpFieldArray";
+import SkillFieldArray from "./SkillFieldArray";
 
 export default function ResumeProfessionalDetails() {
-	return (
-		<section className='bg-white dark:bg-gray-900'>
-			<div className='py-8 px-4 mx-auto max-w-2xl lg:py-16'>
-				<h2 className='mb-4 text-xl font-bold text-gray-900 dark:text-white'>
-					Professional Details
-				</h2>
-				<div>
-					<WorkExpFieldArray></WorkExpFieldArray>
-					<SkillFieldArray></SkillFieldArray>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="mx-auto">
+        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+          Professional Details
+        </h2>
+        <div>
+          <SkillFieldArray></SkillFieldArray>
+          <WorkExpFieldArray></WorkExpFieldArray>
+        </div>
+      </div>
+    </section>
+  );
 }
