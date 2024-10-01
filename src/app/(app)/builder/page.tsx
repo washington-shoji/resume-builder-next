259190e0cc1f7d9654280bc37bbc602345acdfe9
washington-shoji <shoji.washington@gmail.com>
2024-10-01 14:39:29 +1000

    - updated builder page

--- src/app/(app)/builder/page.tsx
+++ src/app/(app)/builder/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ResumeOne from "@/app/components/resome-one/ResumeOne";
 import AccordionWrapper from "@/app/components/AccordionWrapper";
 import { ResumeOneFormInput } from "@/app/types/resume-data.types";
@@ -8,10 +8,12 @@ import { FormProvider, useForm, useWatch } from "react-hook-form";
 import ResumePersonalDetails from "@/app/components/ResumePersonalDetails";
 import ResumeProfessionalDetails from "@/app/components/ResumeProfessionalDetails";
 import ResumeEducationalDetails from "@/app/components/ResumeEducationalDetails";
+import { FaEye, FaSave, FaTrashAlt } from "react-icons/fa";
+import { useReactToPrint } from "react-to-print";
 
 export default function Builder() {
   let storeData: ResumeOneFormInput = {};
-  const [currentStep, setCurrentStep] = useState<number>(0);
+  const componentRef = useRef<HTMLDivElement>(null);
   const [savedFormData, setSavedFormData] = useState<
     ResumeOneFormInput | undefined
   >(undefined);
@@ -21,25 +23,27 @@ export default function Builder() {
     defaultValues: {
       personalData: {
         heading: {
-          fullName: "User Name",
-          roleTitle: "User Role",
-          summary: "User short summary",
+          fullName: "Add Full Name",
+          roleTitle: "Proffesional Role",
+          summary:
+            "Highly motivated software developer with strong experience in backend systems and web application development. Skilled in designing scalable APIs, optimizing performance, and delivering reliable solutions. Passionate about problem-solving, mentoring junior developers, and contributing to open-source projects to promote continuous learning and community collaboration.",
         },
-        contact: [{ iconLabel: "email", contactInfo: "user@email" }],
+        contact: [{ iconLabel: "email", contactInfo: "youremail@email" }],
       },
       professionalData: [
         {
-          company: "User Work Place",
-          timePeriod: "2000 - Present",
-          responsibilities: "User Responsibilities",
+          company: "Work Place or Company",
+          timePeriod: "2020 - Present",
+          responsibilities:
+            "Skilled in designing scalable APIs, optimizing performance, and delivering reliable solutions. Passionate about problem-solving, mentoring junior developers, and contributing to open-source projects to promote continuous learning and community collaboration.",
         },
       ],
       skillData: [{ iconLabel: "node", label: "User Skill" }],
       educationalData: [
         {
-          year: "2000",
-          educationTitle: "User Education",
-          institution: "User Education Institution",
+          year: "2015",
+          educationTitle: "Education Title",
+          institution: "Educational Institution",
         },
       ],
     },
@@ -55,6 +59,21 @@ export default function Builder() {
     localStorage.setItem("form-resume-one-data", JSON.stringify(formData));
   }
 
+  function handleDeleteData(): void {
+    if (
+      window.confirm(
+        "Are you sure you and to delete the Resume Data? It's irreversible!"
+      )
+    ) {
+      localStorage.removeItem("form-data");
+      window.location.reload();
+    }
+  }
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   useEffect(() => {
     storeData = JSON.parse(localStorage.getItem("form-data") as string);
     console.warn("form-resume-one-data", storeData);
@@ -64,11 +83,17 @@ export default function Builder() {
   }, []);
 
   return (
-    <main className="flex justify-center min-h-screen bg-white dark:bg-gray-900">
+    <main className="flex justify-center min-h-screen bg-sky-50 dark:bg-gray-900">
       <div className="grid sm:grid-cols-2 gap-6">
         <FormProvider {...methods}>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className=" flex flex-col gap-4 mt-16 xl:mx-32">
+            <div className=" flex flex-col gap-4 mt-16 xl:mx-32 p-8 shadow-md rounded-lg bg-white">
+              <div>
+                <h1 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+                  Resume builder details
+                </h1>
+              </div>
+
               <AccordionWrapper title="Personal Details">
                 <ResumePersonalDetails></ResumePersonalDetails>
               </AccordionWrapper>
@@ -80,14 +105,43 @@ export default function Builder() {
               <AccordionWrapper title="Education Details">
                 <ResumeEducationalDetails></ResumeEducationalDetails>
               </AccordionWrapper>
+
+              <div className="flex flex-col gap-2">
+                <div className="flex gap-2 mt-4">
+                  <button
+                    className="w-full inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                    type="button"
+                    onClick={handleSubmit(onSubmit)}
+                  >
+                    Save resume data
+                    <FaSave className=" text-xl ml-2" />
+                  </button>
+
+                  <button
+                    className="w-full inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
+                    type="button"
+                    onClick={handleDeleteData}
+                  >
+                    Delete resume data
+                    <FaTrashAlt className=" text-xl ml-2" />
+                  </button>
+                </div>
+
+                <button
+                  className="w-full inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                  type="button"
+                  onClick={handlePrint}
+                >
+                  Preview resume
+                  <FaEye className=" text-xl ml-2" />
+                </button>
+              </div>
             </div>
           </form>
         </FormProvider>
 
-        <div className=" flex flex-col">
-          <div>
-            <ResumeOne formData={formData} />
-          </div>
+        <div className=" flex flex-col bg-white m-16 rounded-md shadow-md p-4">
+          <ResumeOne formData={formData} ref={componentRef} />
         </div>
       </div>
     </main>
